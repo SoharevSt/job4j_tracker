0795cb571bff37b990781a2e6e772dbee08dc819
SoharevSt <devsoharev@yandex.by>
2021-03-23 14:42:28 +0300

    2.1. Реализация класса StartUI [#495961]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -45,6 +45,7 @@ public class Item {
 
     @Override
     public String toString() {
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         return "Item{" +
                 "id=" + id +
