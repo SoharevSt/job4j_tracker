ac717a773b17aeaf0282ca5deff96f7587d50f3c
SoharevSt <devsoharev@yandex.by>
2021-03-24 12:13:24 +0300

    changing the method delete

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -69,7 +69,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (index != -1) {
-            System.arraycopy(items, index + 1, items, index, size - index);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
