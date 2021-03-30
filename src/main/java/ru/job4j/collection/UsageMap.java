47ee7b891eda31fb3856357f371182c8c0056cca
SoharevSt <devsoharev@yandex.by>
2021-03-30 14:08:05 +0300

    0. Map, HashMap [#495936]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        Map<String, String> collection = new HashMap<>();
+        collection.put("123@google.com", "Иванов");
+        collection.put("456@google.com", "Петров");
+        collection.put("789@google.com", "Сидоров");
+        for(String key : collection.keySet()) {
+            System.out.println(key + " = " + collection.get(key));
+        }
+    }
+}
