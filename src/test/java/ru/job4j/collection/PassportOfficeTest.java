aa8716682234a8e5cf87132587095d286d235eb1
SoharevSt <devsoharev@yandex.by>
2021-03-30 14:50:17 +0300

    1. Паспорт и жители [#495937]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenNotUniquePassportThenFalse() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen2);
+        assertThat(office.get("2f44a").getUsername(), is("Petr Arsentev"));
+    }
+}
