bd25a64437d55d2932f4abe2b57648471da58c2c
SoharevSt <devsoharev@yandex.by>
2021-04-02 14:31:08 +0300

    2. Комбинированный компаратор [#495925]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -8,7 +8,6 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
-
     @Test
     public void whenComparatorByName() {
         Comparator<Job> cmpName = new JobSortByName();
