eaecb0f804f6816fbb19e814a8bc402491be3496
SoharevSt <devsoharev@yandex.by>
2021-04-08 14:37:47 +0300

    2. Отсортировать департаменты [#495918]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class DepartmentsTest {
     @Test
@@ -14,7 +14,7 @@ public class DepartmentsTest {
         List<String> input = Arrays.asList("k1/sk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
-        assertThat(result, is(expect));
+        assertEquals(expect, result);
     }
 
     @Test
@@ -27,8 +27,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortAsk() {
-        List<String> input = Arrays.asList("k11", "k2", "k2/sk1", "k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k11");
+        List<String> input = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortAsc(result);
         assertThat(result, is(expect));
@@ -36,8 +36,8 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortDesc() {
-        List<String> input = Arrays.asList("k1", "k2/sk1", "k2", "k1/sk2", "k11", "k1/sk1");
-        List<String> expect = Arrays.asList("k11", "k2", "k2/sk1", "k1", "k1/sk1", "k1/sk2");
+        List<String> input = Arrays.asList("k1", "k2/sk1", "k2", "k1/sk2", "k1/sk1");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk1", "k1/sk2");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
         assertThat(result, is(expect));
