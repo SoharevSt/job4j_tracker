e4b71d0dd5f664552c822fef73457411773af4d7
SoharevSt <devsoharev@yandex.by>
2021-04-23 10:20:36 +0300

    tests

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -2,6 +2,8 @@ package ru.job4j.bank;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -9,19 +11,19 @@ public class BankServiceTest {
 
     @Test
     public void addUser() {
-        User user = new User("3434", "Petr Arsentev");
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
         BankService bank = new BankService();
-        bank.addUser(user);
+        bank.addUser(user.get());
         assertThat(bank.findByPassport("3434"), is(user));
     }
 
     @Test
     public void whenEnterInvalidPassport() {
-        User user = new User("3434", "Petr Arsentev");
+        Optional<User> user = Optional.of(new User("3434", "Petr Arsentev"));
         BankService bank = new BankService();
-        bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertNull(bank.findByRequisite("34", "5546"));
+        bank.addUser(user.get());
+        bank.addAccount(user.get().getPassport(), new Account("5546", 150D));
+        assertEquals(bank.findByRequisite("34", "5546"), Optional.empty());
     }
 
     @Test
